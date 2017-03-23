c615bb7ae526791e173e3e69649dc3ac630670dc
CaoRunze <1206654797@qq.com>
2017-03-23 11:04:38 +0800

    运行时权限

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # ContactsDemo
+##6.0运行时权限问题
+`shouldShowRequestPermissionRationale(Activity activity,String permission)` 
+ 
+ 当一个权限第一次被请求和用户标记过不再提醒的时候,我们写的对话框被展示。 
+   
+ 当用户标记不在提醒时，onRequestPermissionsResult会收到PERMISSION_DENIED，系统询问对话框不展示。
+ 
+ 小米手机点击决拒绝限默认点击不在提醒
+ 
+##CursorLoader
--- app/src/main/java/com/metagem/contactsdemo/MainActivity.java
+++ app/src/main/java/com/metagem/contactsdemo/MainActivity.java
@@ -1,21 +1,29 @@
 package com.metagem.contactsdemo;
 
+import android.Manifest;
 import android.app.LoaderManager;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_CODE_READ_CONTACTS_PERMISSIONS = 1;
     private CursorLoader loader;
 
     @Override
@@ -25,22 +33,29 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         findViewById(R.id.btn_loader).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-                MainActivity.this.startActivityForResult(intent, 1);
+                hasReadContactsPermission();
+
             }
         });
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE_READ_CONTACTS_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                    MainActivity.this.startActivityForResult(intent, 1);
+                } else {
+                    Toast.makeText(MainActivity.this, "READ_CONTACTS Denied", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -87,7 +102,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             } else {
                 hasPhone = "false";
             }
-        /*    if (Boolean.parseBoolean(hasPhone)) {
+            if (Boolean.parseBoolean(hasPhone)) {
                 Cursor phones = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                         ContactsContract.CommonDataKinds.Phone.CONTACT_ID
                                 + " = " + id, null, null);
@@ -95,7 +110,7 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
                     phoneNumber = phones.getString(phones.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                 }
                 phones.close();
-            }*/
+            }
             Log.d(TAG, "onLoadFinished: " + "联系人：" + name + "号码：" + phoneNumber);
         }
     }
@@ -105,5 +120,34 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
 
     }
 
+    public void hasReadContactsPermission() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.READ_CONTACTS)) {
+                //当一个权限第一次被请求和用户标记过不再提醒的时候,我们写的对话框被展示。
+                //最后一种情况，onRequestPermissionsResult 会收到PERMISSION_DENIED ，系统询问对话框不展示。
+                showMessageOKCancel("You need to allow access to Contacts", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CODE_READ_CONTACTS_PERMISSIONS);
+                    }
+                });
+                return;
+            }
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CODE_READ_CONTACTS_PERMISSIONS);
+            return;
+        }
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        MainActivity.this.startActivityForResult(intent, 1);
 
+
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
 }
